d967a6ec0d8124a41fec6baa917b2161c8a49bd7
Marko Lukić <xlukic00@vutbr.cz>
2021-05-16 02:58:08 +0200

    Init project push

--- exercise_3/Program.cs
+++ exercise_3/Program.cs
@@ -6,7 +6,24 @@ namespace exercise_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Matrix a = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 6, 7 }, { 8, 9, 10 } });
+            Matrix b = new Matrix(new double[,] { { 32, 31, 31 }, { 21, 21, 21 }, { 21, 21, 21 } });
+
+            (a + b).ToString();
+            (a - b).ToString();
+            (-a).ToString();
+            (a * b).ToString();
+
+            if (a == b)
+            {
+                Console.WriteLine("a is the same as b");
+            }
+            if (a != b)
+            {
+                Console.WriteLine("a is not the same as b");
+            }
+
+            Console.WriteLine(a.Determinant().ToString());
         }
     }
 }
